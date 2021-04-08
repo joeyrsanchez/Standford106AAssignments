1d160d70d8396234a53b4ed1eebdf907dd415e42
Joey Sanchez <joeyrb.sanchez@gmail.com>
2021-04-07 17:54:01 -0700

    first commit

---
+++ CheckerboardKarel.java
@@ -0,0 +1,27 @@
+package week1;
+/*
+ * File: CheckerboardKarel.java
+ * ----------------------------
+ * When you finish writing it, the CheckerboardKarel class should draw
+ * a checkerboard using beepers, as described in Assignment 1.  You
+ * should make sure that your program works for all of the sample
+ * worlds supplied in the starter folder.
+ */
+
+import stanford.karel.*;
+
+public class CheckerboardKarel extends SuperKarel {
+	
+	public void run() {
+		
+		for(int i=0; i<30; i++) {
+			move();
+			move();
+			turnLeft();
+		}
+		
+	}
+
+}
+/* Test comment*/
+
---
+++ KarelDefendsDemocracy.java
@@ -0,0 +1,49 @@
+package week1;
+import stanford.karel.*;
+
+public class KarelDefendsDemocracy extends SuperKarel {
+
+	public void run() {
+		
+		move();
+		move();
+		move();
+		move();
+		move();	
+		turnLeft();
+		move();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		move();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		pickBeeper();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		
+	
+		}
+		}
+			
+	
+/* test comment */
+
+		
+
